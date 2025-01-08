50b9d303243386eb9d995d9718506502f9edaef0
godsidux <111235317+godsidux@users.noreply.github.com>
2025-01-08 22:30:47 +0530

    Update app.js

--- faq-accordion/app.js
+++ faq-accordion/app.js
@@ -1,19 +1,21 @@
-const buttons = document.querySelectorAll('.btn')
+"use strict";
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', function () {
-        const answer = btn.nextElementSibling;
-        const plusIcon = btn.getElementsByClassName('plus-icon')
-        const minusIcon = this.getElementsByClassName('minus-icon')
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        const plusIcon = btn.querySelector(".plus-icon");
+      const minusIcon = btn.querySelector(".minus-icon");
+    const answer = btn.nextElementSibling;
 
         if (answer.style.maxHeight) {
-            answer.style.maxHeight = null;
-            plusIcon.style.display = 'block';
-            minusIcon.style.display = 'none';
+          answer.style.maxHeight = null;
+          plusIcon.style.display = "block";
+          minusIcon.style.display = "none";
         } else {
-            answer.style.maxHeight = answer.scrollHeight + 'px';
-            plusIcon.style.display = "none";
-            minusIcon.style.display = "block";
+          answer.style.maxHeight = answer.scrollHeight + "px";
+          plusIcon.style.display = "none";
+          minusIcon.style.display = "block";
         }
-    })
-})
+  });
+});
